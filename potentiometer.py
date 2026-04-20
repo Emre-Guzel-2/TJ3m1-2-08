3a367fecc48b4413df569dca790941f51c955738
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2026-04-20 13:41:58 -0400

    Fix variable name
    Corrected variable name from 'pot' to 'pin' and added a print statement on exit.

--- potentiometer.py
+++ potentiometer.py
@@ -1,14 +1,15 @@
 # Created by Emre Guzel
 # Created on April 18, 2026
 # Project: Potentiometer controlled Servo
+
 import board
 import pwmio
 import analogio
 import time
 
-# setting the ports 
+# setting the pin 
 servo = pwmio.PWMOut(board.GP8, frequency=50)
-pot = analogio.AnalogIn(board.GP26)
+pin = analogio.AnalogIn(board.GP26)
 
 # dividing the pot reading by this gives us the angle in degrees
 # biggest 16-bit number/max degrees in the servo
